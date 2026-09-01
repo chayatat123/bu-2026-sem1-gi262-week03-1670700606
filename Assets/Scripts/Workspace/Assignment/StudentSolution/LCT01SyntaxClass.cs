09988a83569c6559762c2552bd56541daaefd460
chayatat123 <chayatat.seer@bumail.net>
2026-09-01 20:10:01 +0700

    work shop

--- Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
@@ -5,7 +5,18 @@ namespace Assignment.StudentSolution.LCT01
 {
     public class Car
     {
-
+        public string Name; 
+        public float Speed;
+        public string Color;
+        public void Move()
+        {
+            Debug.Log("Car is moving");
+            
+        }
+        public void Turn()
+        {
+            Debug.Log("Car is turning");
+        }
     }
 
     public class LCT01SyntaxClass
@@ -13,8 +24,17 @@ namespace Assignment.StudentSolution.LCT01
         public void Start()
         {
             // Student code start HERE ...
+            Car car1 = new Car();
+            car1.Name = "Lamborghini";
+            Debug.Log( car1.Name);
+            car1.Move();
+            car1.Turn();
 
+            Car car2 = new Car();
+            car2.Name = "Toyota";
+            Debug.Log( car2.Name);
             // Student code ends HERE 
         }
+       
     }
 }
--- Assets/Scripts/Workspace/Assignment/StudentSolution/LCT02ClassConstructor.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/LCT02ClassConstructor.cs
@@ -17,26 +17,28 @@ namespace Assignment.StudentSolution.LCT02
 
         // สร้าง constructor ที่รับ parameter 3 ตัว และกำหนดค่าให้กับ properties ของ class
         // โดยทั้ง 3 parameter คือ name, breed, age ตามลำดับ
-        public Dog()
+        public Dog(string MyName, string breed)
         {
-
+            name = MyName;
+            age = 0;
+            this.breed = breed;
         }
 
         /// behaviors ...
 
         public void Bark()
         {
-
+            Debug.Log($"{name} is barking");
         }
 
         public void WagTail()
         {
-
+            Debug.Log($"{name} is wagging tail");
         }
 
         public void StopBarking()
         {
-
+            Debug.Log($"{name} stopped barking");
         }
 
         // end of behaviors ...
@@ -54,7 +56,7 @@ namespace Assignment.StudentSolution.LCT02
 
             // Student code starts HERE ...
             // ...
-
+            dog1 = new Dog("Buddy", "Golden");
             // ...
             // Student code ends HERE ...
 
@@ -63,6 +65,10 @@ namespace Assignment.StudentSolution.LCT02
             dog1.Bark();
             dog1.WagTail();
             dog1.StopBarking();
+
+            Dog dog2 = new Dog("Poopy", "Shih Tzu");
+            dog2.Bark();
         }
+      
     }
 }
